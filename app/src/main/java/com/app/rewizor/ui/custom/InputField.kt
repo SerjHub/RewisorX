f0c624d08cb4006210aaaac00765a68a40005516
Sergey Tarasov <endlessfruit@gmail.com>
2019-08-09 15:11:23 +0300

    todo api

--- app/src/main/java/com/app/rewizor/ui/custom/InputField.kt
+++ app/src/main/java/com/app/rewizor/ui/custom/InputField.kt
@@ -1,8 +1,10 @@
 package com.app.rewizor.ui.custom
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.text.InputType
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
@@ -21,10 +23,31 @@ class InputField @JvmOverloads constructor(
     init {
         context.obtainStyledAttributes(attrRes, R.styleable.InputField).apply {
             if(getBoolean(R.styleable.InputField_inputPassword, false))
-            inputField.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+            inputField.inputType = setInputType(this)
+            Log.i("FindInp", "${inputField.inputType}")
             inputFieldLayout.hint = getString(R.styleable.InputField_inputFieldHint)
             inputField.setText(getString(R.styleable.InputField_inputFieldText))
             recycle()
         }
     }
+
+    private fun setInputType(typedArray: TypedArray) =
+        typedArray.run {
+            when {
+                getBoolean(R.styleable.InputField_inputPassword, false) ->
+                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                getBoolean(R.styleable.InputField_inputEmail, false) ->
+                    InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS or InputType.TYPE_CLASS_TEXT
+                else -> InputType.TYPE_CLASS_TEXT
+            }
+        }
+
+    fun setChecked() {
+        imageCheck.setImageResource(R.drawable.ic_check)
+    }
+
+    fun setUnchecked() {
+        imageCheck.setImageResource(R.drawable.ic_unchecked)
+    }
+
 }
